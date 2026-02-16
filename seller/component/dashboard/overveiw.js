7f4833c8508e25d5b96c43f8adbee9868d33cdf4
mohammednasef97-stack <mohammed.nasef97@gmail.com>
2026-02-16 15:13:17 +0200

    new design

---
+++ seller/component/dashboard/overveiw.js
@@ -0,0 +1,17 @@
+
+
+
+ export function overviewPage() {
+  return `
+    <div class="container mt-4">
+      <div class="row">
+        <div class="col-md-6">
+          <canvas id="salesChart"></canvas>
+        </div>
+        <div class="col-md-6">
+          <canvas id="ordersChart"></canvas>
+        </div>
+      </div>
+    </div>
+  `;
+}
---
+++ seller/overveiw.js
@@ -0,0 +1,25 @@
+
+
+
+
+export function overviewPage() {
+  return `
+    <div class="container mt-4">
+      <div class="row">
+        <div class="col-md-4">k
+          <div class="card p-3">j
+            <h5>Total Sales</h5>
+            <p>1200$</p>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card p-3">
+            <h5>Total Orders</h5>
+            <p>320</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+}
