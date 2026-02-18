21a1034a8b9afae005aa381043ccd956f0f9b75f
Eng-Ayman-Mohamed <eng.ayman.mohamed.abotaha.kasim@gmail.com>
2026-02-18 19:16:05 +0200

    DB Models

---
+++ DataBase/models/Product.js
@@ -0,0 +1,52 @@
+import { requireFields } from "../utils/validator.js";
+
+export default class Product {
+  constructor({
+    vendorId,
+    title,
+    desc,
+    category,
+    price,
+    discount,
+    rating,
+    stock,
+    weight,
+    images,
+    featured,
+    approved,
+  }) {
+    requireFields({ vendorId, title, price, category, stock }, [
+      "vendorId",
+      "title",
+      "price",
+      "category",
+      "stock",
+    ]);
+    //  Core
+    this.id = Date.now() || generateProductId();
+    this.createdAt = Date.now();
+    this.vendorId = vendorId;
+    this.title = title;
+    this.category = category;
+    this.stock = Number(stock);
+
+    this.desc = desc || "";
+    this.price = Number(price);
+    this.discount = Number(discount) || 0;
+    this.rating = Number(rating) || 0;
+    this.weight = weight || 0;
+    this.images = images || [];
+
+    //  Flags
+    this.featured = featured ?? false;
+    this.approved = approved ?? false;
+  }
+
+  getFinalPrice() {
+    return this.price - (this.price * this.discount) / 100;
+  }
+
+  isInStock() {
+    return this.stock > 0;
+  }
+}
